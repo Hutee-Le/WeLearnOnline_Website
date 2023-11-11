e163ad9dabe87123d428cf632a89582ead23b21c
Minh Nhut <lenhut0929@gmail.com>
2023-11-11 15:16:05 +0700

    Add Email Setting, EmailService, IEmailService,
    Cấu hình EmailSetting, đăng ký dịch vụ EmailService, cài đặt thư viện Mailkit

--- WeLearnOnine_Website/WeLearnOnine_Website/Controllers/HomeController.cs
+++ WeLearnOnine_Website/WeLearnOnine_Website/Controllers/HomeController.cs
@@ -2,19 +2,22 @@
 using System.Diagnostics;
 using WeLearnOnine_Website.Models;
 using WeLearnOnine_Website.Repositories;
+using WeLearnOnine_Website.Services;
 
 namespace WeLearnOnine_Website.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private IFavListRepository _favListRepository;
+        private readonly IFavListRepository _favListRepository;
         private readonly ICourseRepository _courseRepository;
-        public HomeController(ILogger<HomeController> logger, IFavListRepository favListRepository, ICourseRepository courseRepository)
+        private readonly IEmailService _emailService;
+        public HomeController(ILogger<HomeController> logger, IFavListRepository favListRepository, ICourseRepository courseRepository, IEmailService emailService)
         {
             _logger = logger;
             _favListRepository = favListRepository;
             _courseRepository = courseRepository;
+            _emailService = emailService;
         }
 
         public IActionResult Index()
@@ -32,6 +35,16 @@ namespace WeLearnOnine_Website.Controllers
         public IActionResult FavList()
         {
             return View(_favListRepository.GetAllByUserId(1));
+        }
+
+        public IActionResult SendMail()
+        {
+            var toAddresses = new List<string> { "lenhut0929@gmail.com", "lenhut2909@gmail.com" };
+
+            // Gửi email đến nhiều địa chỉ
+            _emailService.SendEmailAsync(toAddresses, "Sending test email for Project Welearn", "Đây là mail test bạn có thể bỏ qua mail này.");
+
+            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
